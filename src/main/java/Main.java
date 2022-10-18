883550482166c909d4dfb0d8dd72f8ab6b534b13
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-10-18 20:11:58 +0300

    added code with preparedStatement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,22 @@ import java.sql.SQLException;
 
 public class Main {
 
-    private static final String CREATE_TABLE = """
-         CREATE TABLE IF NOT EXISTS user (
-         id int PRIMARY KEY AUTO_INCREMENT,
-         first_name varchar(50),
-         last_name varchar(50),
-         age int)
-          """;
-
+// Написати код який змінить fist_name другому(якщо 1 юзер, то першому) юзеру
 
     public static void main(String[] args) {
 
         try (var connection = DriverManager.getConnection(JDBCConstants.URL, JDBCConstants.USERNAME, JDBCConstants.PASSWORD)) {
-            var statement = connection.createStatement();
 
-            statement.execute(CREATE_TABLE);
+            var statement = connection.prepareStatement(SqlQueries.UPDATE_USER_AGE);
+            var prepareStatement = connection.prepareStatement(SqlQueries.UPDATE_USER_AGE);
 
+            statement.execute(SqlQueries.CREATE_TABLE);
+//          statement.execute(SqlQueries.INSERT_USERS);
+
+            prepareStatement.setInt(1, 100);
+            prepareStatement.setInt(2, 1);
+
+            prepareStatement.executeUpdate();
 
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
---
+++ src/main/java/SqlQueries.java
@@ -0,0 +1,26 @@
+public final class SqlQueries {
+
+    public static final String CREATE_TABLE = """
+        CREATE TABLE IF NOT EXISTS user (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        first_name varchar(50),
+        last_name varchar(50),
+        age int)
+         """;
+
+    public static final String INSERT_USERS = """
+        INSERT INTO user (first_name, last_name,age) VALUES 
+        ('john', 'snow',42),
+        ('max', 'pax',18),
+        ('tirion', 'lanister',30)
+        """;
+
+    public static final String UPDATE_USER_AGE = """
+        UPDATE user SET age=? WHERE id =?
+        """;
+
+
+    private SqlQueries() {
+    }
+
+}
